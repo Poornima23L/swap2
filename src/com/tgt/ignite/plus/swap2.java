bdc4f5a2c7274f69f5909c81f8d00b5318804685
Vinay Kumar Muniraju <poonima23l@gmail.com>
2020-03-24 22:53:07 +0530

    message

---
+++ src/com/tgt/ignite/plus/swap2.java
@@ -0,0 +1,15 @@
+package com.tgt.ignite.plus;
+
+public class swap2 {
+    public static void main(String[] args){
+        int a,b,t;
+        a=10;
+        b=20;
+        t=b;
+        b=a;
+        a=t;
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+
+    }
+}
